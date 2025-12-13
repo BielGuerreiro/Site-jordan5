324cc32500e76aeabbda3e8cde41c229999305a9
Guerreiro <Bielguerreiro2526@gmail.com>
2025-12-13 16:13:36 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,62 @@
+const slides = document.querySelectorAll(".slider-item");
+const indicators = document.querySelectorAll(".indicator");
+let currentSlide = 0;
+const slideInterval = 3000;
+
+function nextSlide() {
+  slides[currentSlide].classList.remove("active");
+  indicators[currentSlide].classList.remove("active");
+
+  currentSlide = (currentSlide + 1) % slides.length;
+
+  slides[currentSlide].classList.add("active");
+  indicators[currentSlide].classList.add("active");
+}
+
+let slideTimer = setInterval(nextSlide, slideInterval);
+
+indicators.forEach((indicator, index) => {
+  indicator.addEventListener("click", () => {
+    clearInterval(slideTimer);
+
+    slides[currentSlide].classList.remove("active");
+    indicators[currentSlide].classList.remove("active");
+
+    currentSlide = index;
+    slides[currentSlide].classList.add("active");
+    indicators[currentSlide].classList.add("active");
+
+    slideTimer = setInterval(nextSlide, slideInterval);
+  });
+});
+
+const trilhoProdutos = document.querySelector(".slider-wrapper");
+const slidesProdutos = document.querySelectorAll(".produto-slide");
+const btnAnteriorProd = document.querySelector(".esquerda");
+const btnProximoProd = document.querySelector(".direita");
+
+let indiceProdutoAtual = 0;
+
+function moverCarrosselProdutos() {
+  trilhoProdutos.style.transform = `translateX(${-indiceProdutoAtual * 100}%)`;
+}
+
+btnProximoProd.addEventListener("click", () => {
+  indiceProdutoAtual++;
+
+  if (indiceProdutoAtual > slidesProdutos.length - 1) {
+    indiceProdutoAtual = 0;
+  }
+
+  moverCarrosselProdutos();
+});
+
+btnAnteriorProd.addEventListener("click", () => {
+  indiceProdutoAtual--;
+
+  if (indiceProdutoAtual < 0) {
+    indiceProdutoAtual = slidesProdutos.length - 1;
+  }
+
+  moverCarrosselProdutos();
+});
